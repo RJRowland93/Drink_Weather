15a0cf200d4afa843cfa770984ee845c28734313
michaeledwards91 <michael.edwards1991@gmail.com>
2016-11-17 15:24:23 -0600

    Assigned global variables

--- assets/javascript/backend.js
+++ assets/javascript/backend.js
@@ -1,9 +1,13 @@
+//Global Variables
+var weather = ""; //weather condition of queried city/zip
+var temperatureF; //temperature in fahrenheit, will hold temp of queried city/zip
+//Weather API call parameters
 var weatherAPIKey = "APPID=4888c0dfdc6bd6c82a0adf3d3cef0ba3";
 var city = "&q=";
 var zipcode = "&zip=";
-
+//Query URL
 var weatherQuery = "api.openweathermap.org/data/2.5/weather?"+weatherAPIKey;
-
+//Append appropriate parameters depending on user input
 if (/*input is city*/) {
 	city += /*input*/;
 	weatherQuery += city;
