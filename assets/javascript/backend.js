b1abedff8b4ed879e1b6def32d110e9064736970
june.k.lara@gmail.com <june.k.lara@gmail.com>
2016-11-17 15:21:54 -0600

    test override

--- assets/javascript/backend.js
+++ assets/javascript/backend.js
@@ -1,5 +1,5 @@
 var weatherAPIKey = "APPID=4888c0dfdc6bd6c82a0adf3d3cef0ba3";
-var city = "&q=";
+var town = "&q=";
 var zipcode = "&zip=";
 
 var weatherQuery = "api.openweathermap.org/data/2.5/weather?"+weatherAPIKey;
