09a3f5dc187d82e4b6c949090ac7ef06549a019f
michaeledwards91 <michael.edwards1991@gmail.com>
2016-11-17 14:52:23 -0600

    Added .gitignore. Renamed JS file, built weather API call skeleton

---
+++ assets/javascript/backend.js
@@ -0,0 +1,21 @@
+var weatherAPIKey = "APPID=4888c0dfdc6bd6c82a0adf3d3cef0ba3";
+var city = "&q=";
+var zipcode = "&zip=";
+
+var weatherQuery = "api.openweathermap.org/data/2.5/weather?"+weatherAPIKey;
+
+if (/*input is city*/) {
+	city += /*input*/;
+	weatherQuery += city;
+} else if (/*input is zip*/) {
+	zipcode += /*input*/;
+	weatherQuery += zipcode;
+}
+
+$.ajax({
+	url: weatherQuery,
+	method: "GET"
+}).done(function(response) {
+	//do stuff
+	console.log(response);
+})
