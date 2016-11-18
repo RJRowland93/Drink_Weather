ce24309bf58daf8fab9a2ab78242bd6bea5cfe42
june.k.lara@gmail.com <june.k.lara@gmail.com>
2016-11-18 13:20:07 -0600

    Created drink object with array of objects each is a drink with id

--- assets/javascript/backend.js
+++ assets/javascript/backend.js
@@ -11,29 +11,73 @@ var unitsParam = "&units=imperial";
 var weatherQuery = "api.openweathermap.org/data/2.5/weather?"+weatherAPIKey+unitsParam;
 
 //Append appropriate parameters depending on user input
-if (/*input is city*/) {
-	cityParam += /*input*/;
-	weatherQuery += cityParam;
-} else if (/*input is zip*/) {
-	zipcodeParam += /*input*/;
-	weatherQuery += zipcodeParam;
-}
+//input id is search
+//submit button
 
-$.ajax({
-	url: weatherQuery,
-	method: "GET"
-}).done(function(response) {
-	//do stuff
-	console.log(response);
-});
+$("#submitButton")/*?*/.on("click", function() {
 
+	var userInput = $("#search").val().trim();
+	console.log(userInput);
+	//boolean variable to determine whether to query with city param or zip param
+	var isCity;
+
+	var parsedInput = parseInt(userInput);
+	console.log(parsedInput);
+	if (parsedInput === NaN) {
+		isCity = true;
+	} else {
+		isCity = false;
+	}
+
+	if (isCity) {
+		cityParam += userInput;
+		weatherQuery += cityParam;
+	} else {
+		zipcodeParam += userInput;
+		weatherQuery += zipcodeParam;
+	}
+
+	console.log(weatherQuery);
+
+	$.ajax({
+		url: weatherQuery,
+		method: "GET"
+	}).done(function(response) {
+		console.log(response);
+		weather = response.weather.main;
+		temperatureF = response.main.temp;
+	})
+
+	console.log(weather);
+	console.log(temperatureF);
+
+})
 
 
 
 
 // Decide which drink is appropriate for current weather.
-// Query cocktail API for that drink.
-// Display relevant cocktail info
+//still need to add the if statments to select drink array acording to weather and also a random number for index of the array 
+
+
+// oboject for drinks
+
+var drinks = {
+ 
+	"summer": 	[{ "name": "110 in the Shade", "id": "15423" }, {"name": "151 Florida Bushwacker" , "id": "14588",}, {"name": "Ultimate Margarita" , "id": "14622",}, {"name": "Bloody Mary", "id": "11113" ,}, {"name": "A Gilligan's Island", "id": "16943" }],
+
+	"fall": 	[{"name": "Arctic Fish": , "id": "14622",} ,{"name": "Apricot punch" , "id": "15849" ,}, {"name": "Bloody Mary", "id": "11113" ,}, {"name": "Archbishop" , "id": "11052" ,}, {"name": "Bluebird" , "id": "11120" }],
+
+	"spring": 	[{"name": "Loch Lomond" , "id": "11658" ,}, {"name": "Bloody Mary", "id": "11113" ,}, {"name": "English Rose Cocktail" , "id": "11339" ,}, {"name": "Gin Daisy" , "id": "11408",}, {"name": "Grass Skirt" , "id": "11433"}],
+
+	"winter": 	[ {"name": "Black Russian" , "id": "11102" ,}, {"name": "Cosmopolitan Martini" , "id": "14133" ,}, {"name": "Affinity" , "id": "11009" ,}, {"name": "Balmoral" , "id": "11060" ,}, {"name": "Cafe Savoy" , "id": "14181" ,}]
+
+
+
+
+}
+
+
 
 
 //ajax call for coktaildb
